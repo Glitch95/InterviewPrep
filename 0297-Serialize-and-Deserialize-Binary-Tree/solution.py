0608f943e75044a57edb48b5bdf9e6a56b7cfda9
Kalesh-Singh <kaleshsingh96@gmail.com>
2019-09-15 20:13:45 -0400

    297. Serialize and Deserialize Binary Tree

---
+++ 0297-Serialize-and-Deserialize-Binary-Tree/solution.py
@@ -0,0 +1,75 @@
+from collections import deque
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+
+        :type root: TreeNode
+        :rtype: str
+        """
+        if not root:
+            return ''
+
+        queue, result = deque(), []
+        queue.append(root)
+
+        while queue:
+            node = queue.popleft()
+            if node:
+                queue.append(node.left)
+                queue.append(node.right)
+                result.append(str(node.val))
+            else:
+                result.append('null')
+
+        return ','.join(result)
+
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+
+        :type data: str
+        :rtype: TreeNode
+        """
+        if data == '':
+            return None
+
+        vals = data.split(',')
+
+        queue, root = deque(), TreeNode(int(vals[0]))
+        queue.append(root)
+
+        i, n = 1, len(vals)
+        while i < n:
+            node = queue.popleft()
+            val = vals[i]
+            if val == 'null':
+                node.left = None
+            else:
+                node.left = TreeNode(int(val))
+                queue.append(node.left)
+            i += 1
+
+            if i < n:
+                val = vals[i]
+                if val == 'null':
+                    node.right = None
+                else:
+                    node.right = TreeNode(int(val))
+                    queue.append(node.right)
+            i += 1
+
+        return root
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.deserialize(codec.serialize(root))
+
